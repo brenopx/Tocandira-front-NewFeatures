dc1e6c811aa6a11b1097ddff23acc8933969d206
brenopx <bpereira@aimirimsti.com.br>
2023-11-24 16:51:13 -0300

    feat(component/Popups): Adding an add new lines button and saving the added lines

--- src/component/Popups/EditPointFullScreen.js
+++ src/component/Popups/EditPointFullScreen.js
@@ -21,7 +21,7 @@ import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import {
     GridActionsCellItem,
 } from '@mui/x-data-grid';
-import { Box } from '@mui/material';
+import { Button, Stack, TextField } from '@mui/material';
 //import './DataSourcePopup.css';
 
 // #######################################
@@ -38,11 +38,12 @@ class DataSourcePopup extends React.PureComponent {
     /** Defines the component state variables */
     state = {
         dp_content:[],
+        NameButtonNewRows:"Add New Rows"
     };
     
-    handleDsContent=() => {
+    handleDsContent= () =>{
         let list_dp = this.props.datapoint.dp_content.filter(this.filterData)
-        list_dp.forEach((row) => {
+        list_dp.forEach((row)=>{
             row['row_state'] = 'no_alterations'
         })
         const newState = {...this.state};
@@ -50,14 +51,18 @@ class DataSourcePopup extends React.PureComponent {
         this.setState(newState);
     }
 
-    processRowUpdate=(newRow) =>{
+    processRowUpdate= (newRow) =>{
         let new_dp_content = []
-        let list_dp = this.state.dp_content
-        list_dp.forEach((row) => {
-            if(row.name === newRow.name){
-                newRow['access'] = row['access']
-                newRow['row_state'] = 'Edited'
-                new_dp_content.push(newRow)
+        let list_dp = JSON.parse(JSON.stringify(this.state.dp_content))
+        list_dp.forEach( (row) =>{
+            if(row.table_id === newRow.table_id){
+                if(row.row_state === 'NewRow'){
+                    new_dp_content.push(newRow)
+                } else {
+                    newRow['access'] = row['access']
+                    newRow['row_state'] = 'Edited'
+                    new_dp_content.push(newRow)
+                }
             } else {
                 new_dp_content.push(row)
             }
@@ -68,13 +73,13 @@ class DataSourcePopup extends React.PureComponent {
         return (newRow);
     }
 
-    handleClear=() =>{
+    handleClear= () =>{
         const newState = {...this.state};
         newState.dp_content = [];
         this.setState(newState);
     }
 
-    handleOkClickDialog=() => {
+    handleOkClickDialog= () =>{
         this.state.dp_content.forEach((row)=>{
             if(row.row_state === 'Delete'){
                 this.props.onDeleteDataPoint(
@@ -86,68 +91,110 @@ class DataSourcePopup extends React.PureComponent {
                     this.props.global.backend_instance, 
                     row
                 );
+            }else if(row.row_state === 'NewRow'){
+                this.props.onNewSave(
+                    this.props.global.backend_instance,
+                    row
+                );
             }
         });
         this.props.onHandleStateEditDp(false);
         this.handleClear();
     }
 
-    handleCancelClickDialog=() => {
+    handleCancelClickDialog= () =>{
         this.props.onHandleStateEditDp(false);
         this.handleClear();
     }
 
-    handleProcessRowUpdateError=(error) => {
+    handleProcessRowUpdateError= (error) => {
         console.log("valor do error", error);
+    }
+
+    handleRestoreClick= (params) => () =>{
+        let row_defaults = JSON.parse(JSON.stringify(this.props.datapoint.dp_defaults['Siemens']));
+        let copy_ds_content = JSON.parse(JSON.stringify(this.state.dp_content));
+        let list_ds_no_alterations = this.props.datapoint.dp_content.filter(this.filterData)
+        let new_list_dp = []
+        let row_no_alterations = undefined
+        list_ds_no_alterations.forEach( (row) => {
+            if(row.table_id === params.id){
+                row_no_alterations = row
+                row_no_alterations['row_state'] = 'no_alterations'
+            }
+        })
+        new_list_dp = copy_ds_content.map( (row) => {
+            if(row_no_alterations !== undefined && row_no_alterations.table_id === row.table_id){
+                return (row_no_alterations)
+            } else if(row.row_state === "NewRow" && row.table_id === params.id){
+                let row_empty = row
+                row_empty.name = row_defaults.name
+                row_empty.description = row_defaults.description
+                row_empty.datasource_name = row_defaults.datasource_name
+                row_empty.num_type = row_defaults.num_type.defaultValue
+                row_empty.timeout = row_defaults.timeout
+                return(row_empty)
+            } else {
+                return(row)
+            }
+        })
+        const newState = {...this.state};
+        newState.dp_content = new_list_dp;
+        this.setState(newState);
+    };
+    
+    handleDeleteClick= (params) => () =>{
+        let copy_dp_content = JSON.parse(JSON.stringify(this.state.dp_content));
+        let newlist_dp = []
+        if(params.row.row_state === "NewRow"){
+            copy_dp_content.map( (row) => {
+                if(row.table_id === params.id){
+                    return(row)
+                }
+                newlist_dp.push(row)
+                return(row)
+            })
+        } else {
+            copy_dp_content.map( (row) => {
+                if(row.table_id === params.id){
+                    row['row_state'] = 'Delete'
+                }
+                newlist_dp.push(row)
+                return(row)
+            })
+        }
+        const newState = {...this.state};
+        newState.dp_content = newlist_dp;
+        this.setState(newState);
+    };
+
+    getRowClassName= (params) =>{
+        return(params.row.row_state)
+    }
+
+    HandleClickButtonNewRows= () =>{
+        let copy_dp_content = JSON.parse(JSON.stringify(this.state.dp_content));
+        let new_row = JSON.parse(JSON.stringify(this.props.datapoint.dp_defaults['Siemens']));
+        new_row.table_id = this.state.dp_content.length
+        new_row.num_type = new_row.num_type.defaultValue
+        new_row.row_state = 'NewRow'
+        let new_list_dp = [...copy_dp_content, new_row]
+        const newState = {...this.state};
+        newState.dp_content = new_list_dp
+        this.setState(newState);
     }
 
     /** Description.
     * @param ``: 
     * @returns */
-    filterData=(row) => {
+    filterData= (row) =>{
         const ds_name = row.datasource_name
         const ds = this.props.datasource.ds_content.filter(row=>row.name===ds_name)[0]
-        if (ds) {
+        if(ds){
             return(row.active && ds.collector_id===this.props.collector.selected.id)
-        } else {
+        } else  {
             return(false)
         }
-    }
-
-    handleRestoreClick = (params) => () => {
-        let new_row = []
-        let list_dp_no_alterations = this.props.datapoint.dp_content.filter(this.filterData)
-        list_dp_no_alterations.forEach((row_no_alterations) => {
-            if(row_no_alterations.name === params.id){
-                new_row = row_no_alterations
-                new_row['row_state'] = 'no_alterations'
-            }
-        })
-        let list_dp = this.state.dp_content.map((row) => {
-            if(row.name === params.id){
-                row = new_row
-                return(row)
-            } else {return (row)}
-        })
-        const newState = {...this.state};
-        newState.dp_content = list_dp;
-        this.setState(newState);
-    };
-    
-    handleDeleteClick = (params) => () => {
-        let list_dp = this.state.dp_content.map((row) => {
-            if(row.name === params.id){
-                row['row_state'] = 'Delete'
-            }
-            return(row)
-        })
-        const newState = {...this.state};
-        newState.dp_content = list_dp;
-        this.setState(newState);
-    };
-
-    getRowClassName=(params) => {
-        return (params.row.row_state)
     }
 
     /** Defines the component visualization.
@@ -158,20 +205,20 @@ class DataSourcePopup extends React.PureComponent {
             getActions: (params) => {
                 return[<GridActionsCellItem
                     icon={<RestoreIcon />}
-                    disabled={params.row.row_state === "Delete" || params.row.row_state === "Edited" ? false : true}
+                    disabled={params.row.row_state === "Delete" || params.row.row_state === "Edited" || params.row.row_state === "NewRow" ? false : true}
                     label="Edit"
                     onClick={this.handleRestoreClick(params)}
                     color="inherit"
                 />,
                 <GridActionsCellItem
                     icon={<DeleteIcon />}
-                    disabled={params.row.row_state === "no_alterations" || params.row.row_state === "Edited" ? false : true}
+                    disabled={params.row.row_state === "no_alterations" || params.row.row_state === "Edited" || params.row.row_state === "NewRow" ? false : true}
                     label="Delete"
                     onClick={this.handleDeleteClick(params)}
                     color="inherit"
                 />,
             ]},},
-            {field: "name", headerName:"Name",editable: false, flex:1},
+            {field: "name", headerName:"Name",editable: true, flex:1},
             {field: "description", headerName:"Description",editable: true, flex:1},
             {field: "num_type", headerName:"Num Type",editable: true, flex:1},
             {field: "datasource_name", headerName:"Data Source",editable: true, flex:1},
@@ -187,18 +234,20 @@ class DataSourcePopup extends React.PureComponent {
                     onOkClick={this.handleOkClickDialog}
                     onCancelClick={this.handleCancelClickDialog}
                 >
-                    <Box
+                    <Stack
                         sx={{
                             '& .Delete': {
-                                backgroundColor: '#ff6961',
+                                backgroundColor: '#f8d2d0',
                                 color: '#000',
                             },'& .Edited': {
                                 backgroundColor: '#ffffd1',
                                 color: '#000',
-                            },'& .New_Row': {
-                                backgroundColor: '#81ff8a',
+                            },'& .NewRow': {
+                                backgroundColor: '#d7f9d6',
                                 color: '#000',
-                            }
+                            },
+                            'display': 'flex',
+                            'direction': 'row'
                         }}
                     >
                         <EditTable 
@@ -208,13 +257,20 @@ class DataSourcePopup extends React.PureComponent {
                             handleProcessRowUpdateError={this.handleProcessRowUpdateError}
                             getRowClassName={(params) => this.getRowClassName(params)}
                         />
-                    </Box>
+                        <Stack direction='row' spacing='1rem' marginTop='1rem' alignSelf='start'>
+                        <Button variant="contained" onClick={this.HandleClickButtonNewRows} >
+                            {this.state.NameButtonNewRows}
+                        </Button>
+                        <TextField>
+                        </TextField>
+                    </Stack>
+                    </Stack>
                 </DialogFullScreen>
         );
         return(jsx_component);
     }
 
-    componentDidMount=() => {
+    componentDidMount= () =>{
         this.handleDsContent();
     }
 }
@@ -230,10 +286,11 @@ const reduxStateToProps = (state) =>({
 
 /** Map the Redux actions dispatch to some component props */
 const reduxDispatchToProps = (dispatch) =>({
-    onHandleStateEditDp:(state)=>dispatch(datapointActions.handleStateEditDp(state)),
-    onEditSave:(api,info)=>dispatch(datapointActions.putData(api,info)),
-    onDeleteDataPoint:(api,dp_name)=>dispatch(datapointActions.deleteData(api,dp_name)),
+    onHandleStateEditDp: (state) => dispatch(datapointActions.handleStateEditDp(state)),
+    onEditSave: (api,info) => dispatch(datapointActions.putData(api,info)),
+    onDeleteDataPoint: (api,dp_name) => dispatch(datapointActions.deleteData(api,dp_name)),
+    onNewSave:(api,info)=>dispatch(datapointActions.pushData(api,info)),
 });
 
 // Make this component visible on import
-export default connect(reduxStateToProps,reduxDispatchToProps)(DataSourcePopup);
+export default connect(reduxStateToProps, reduxDispatchToProps)(DataSourcePopup);
